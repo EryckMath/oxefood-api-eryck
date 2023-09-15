fe8f3aaab04ed7839aefc1d36e70fa841b4f90b3
aluno <aluno@ifpe>
2023-09-15 17:18:44 -0300

    att sexta15

---
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
@@ -0,0 +1,47 @@
+package br.com.ifpe.oxefood.api.produto;
+
+//import java.time.LocalDate;
+
+//import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.ifpe.oxefood.modelo.produto.Produto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProdutoRequest {
+
+    private String titulo;
+
+    //@JsonFormat(pattern = "dd/MM/yyyy")
+    //private LocalDate dataNascimento;
+
+    private String codigo;
+
+    private String descricao;
+
+    private String valorUnit;
+
+    private String tempoEntregaMin;
+
+    private String tempoEntregaMax;
+
+    public Produto build() {
+
+        return Produto.builder()
+                .titulo(titulo)
+                .codigo(codigo)
+                .descricao(descricao)
+                .valorUnit(valorUnit)
+                .tempoEntregaMin(tempoEntregaMin)
+                .tempoEntregaMax(tempoEntregaMax)
+                .build();
+    }
+
+}
